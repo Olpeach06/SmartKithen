e70bd0bb3d31330ca9e5272aac5907d95e07ff87
User <User@HOME-PC>
2025-10-11 00:44:54 +0300

    Добавьте файлы проекта.

---
+++ SmartKithen/Pages/HomePage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmartKithen.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Registration());
+        }
+
+        private void GuestButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Authorization());
+        }
+
+        private void AboutBt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HelpBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SmartKithen/Pages/UserInformation.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SmartKithen;
+
+namespace SmartKithen.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для UserInformation.xaml
+    /// </summary>
+    public partial class UserInformation : Page
+    {
+        public UserInformation()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnChangePassword_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnThemeToggle_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnThemeToggle_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnBackup_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRestore_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnExport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDeleteAccount_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
